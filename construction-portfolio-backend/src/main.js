7893cbbb85cb75c062ac154d030da8118b9565eb
Alexander0131 <acidinme0131@gmail.com>
2026-07-19 08:26:03 +0000

    Dns disable

--- construction-portfolio-backend/src/main.js
+++ construction-portfolio-backend/src/main.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const dns = require("dns");
+// const dns = require("dns");
 
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const mongoose = require("mongoose");
 const app = require("./app");
