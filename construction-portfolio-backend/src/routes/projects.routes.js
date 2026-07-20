4f63f86d68a5340e085d4869df571fc8918338b4
Alexander0131 <acidinme0131@gmail.com>
2026-07-20 19:52:25 +0000

    pro 2

--- construction-portfolio-backend/src/routes/projects.routes.js
+++ construction-portfolio-backend/src/routes/projects.routes.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const requireAdmin = require("../middleware/requireAdmin");
-// const {
-//   getProjects,
+const {
+  getProjects,
 //   createProject,
 //   getSingleProjects,
 //   updateProject,
 //   deleteProject,
-// } = require("../controllers/projects.controller");
+} = require("../controllers/projects.controller");
 
 const router = express.Router();
 
 // // Public reads
-// router.get("/", getProjects);
+router.get("/", getProjects);
 // router.get("/:id", getSingleProjects);
 
 // // Admin-only writes
