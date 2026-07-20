0955bd03829873636ca59a345d3cf4eb689e70ba
Alexander0131 <acidinme0131@gmail.com>
2026-07-20 19:05:48 +0000

    clerk restore

--- construction-portfolio-backend/index.js
+++ construction-portfolio-backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 // const dotenv = require("dotenv");
 const cors = require("cors");
 // const fileUpload = require("express-fileupload");
-// const { clerkMiddleware } = require("@clerk/express");
+const { clerkMiddleware } = require("@clerk/express");
 // dotenv.config();
 // const dns = require("dns");
 
@@ -47,7 +47,7 @@ app.use(
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
-// app.use(clerkMiddleware());
+app.use(clerkMiddleware());
 
 // app.use(
 //   fileUpload({
